05c3fbd39b46264c9aee5fa64b35ee8867794697
shubhamiscodding <shubham.modi.cg@gmail.com>
2025-02-20 16:51:43 +0530

    clone

---
+++ src/parts/Footer.jsx
@@ -0,0 +1,76 @@
+let Footer = function (){
+    return(
+        <>
+         <div className="footer-container">
+      <div className="footer-section app-download">
+        <h1>Download Progcap App</h1>
+        <div className="store-links">
+          <img src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/619b61d6d963e56d49b43b1d_App%20Store%20Button%20Small.svg" alt="Apple Store" />
+          <img src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/619b61d7a8289a992f22acfc_Play%20Store%20Button%20Small.svg" alt="Google Play Store" />
+        </div>
+      </div>
+
+      <div className="footer-section about">
+        <h2>PROGCAP</h2>
+        <p>
+          Progcap (Desiderata Impact Ventures Private Limited) is an award-winning financial technology firm that aims to revolutionize the path of providing financial access to underserved segments of the Indian retail economy and empower promising enterprises with capital for progress.
+        </p>
+        <p>
+          We work across the entire retail ecosystem and supply chain with our end-to-end financial services and solutions.
+        </p>
+        <img src="https://cdn.prod.website-files.com/6193782af8f15b5c5763d1de/62ceb382a47f621908b18930_iso.webp" alt="ISO Certification" className="iso-logo" />
+      </div>
+
+      <div className="footer-section links">
+        <h3>Useful Links</h3>
+        <ul>
+          <li>About</li>
+          <li>We are hiring</li>
+          <li>Life @ Progcap</li>
+          <li>Contact</li>
+          <li>FAQ</li>
+          <li>T&amp;C</li>
+          <li>Pricing</li>
+          <li>Privacy Policy</li>
+          <li>Refund Policy</li>
+        </ul>
+      </div>
+
+      <div className="footer-section partners">
+        <h3>Partners</h3>
+        <ul>
+          <li>Progfin Private Limited</li>
+          <li>Kisetsu Saison Finance (India) Pvt Ltd</li>
+          <li>Cholamandalam Investment</li>
+          <li>Muthoot Finance Ltd</li>
+          <li>Ujjivan Small Finance Bank Ltd</li>
+        </ul>
+      </div>
+
+      <div className="footer-section contact">
+        <h3>Desiderata Impact Ventures Private Limited</h3>
+        <p>+91-8929124124</p>
+        <p>info@progcap.com</p>
+        <p>For HR related queries/info: HR@progcap.com</p>
+        <p>
+          1st Floor, C-3, Block C, Qutab Institutional Area, New Delhi, Delhi-110016
+        </p>
+        <p>CIN - U74999DL2017PTC310632</p>
+        <h4>Grievance Redressal Officer</h4>
+        <p>Name: Jeetendra Singh Patwal</p>
+        <p>Contact: +91-7669302039</p>
+        <p>Email: jeetendra.patwal@progcap.com</p>
+        <p><a href="#">Grievance Redressal Details</a></p>
+      </div>
+
+      <div className="footer-section social">
+        <a href="#"><i className="fab fa-facebook"></i></a>
+        <a href="#"><i className="fab fa-twitter"></i></a>
+        <a href="#"><i className="fab fa-youtube"></i></a>
+      </div>
+    </div>
+        </>
+    )
+}
+
+export default Footer;
